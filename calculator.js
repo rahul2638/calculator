10d4c234f763d0d6577dd1391a2e0a5f1f645d9d
rahul2638 <107794349+rahul2638@users.noreply.github.com>
2022-10-15 09:42:12 +0530

    new button added

--- calculator.js
+++ calculator.js
@@ -1,1 +1,19 @@
 console.log('hello world');
+
+var display = document.getElementById('display'),
+    clear = document.getElementsByClassName('clbutton'),
+    back = document.getElementsByClassName('bkbutton'),
+    operator = document.getElementsByClassName('opbutton'),
+    num = document.getElementsByClassName('numbutton'),
+    equal = document.getElementsByClassName('eqbutton'),
+    currentnumValue,
+    currentOperator,
+    displayValue,
+    result,
+    backspaceValue;
+
+
+function dataInput() {
+    currentnumValue = this.value;
+    display.value += currentnumValue;
+}
